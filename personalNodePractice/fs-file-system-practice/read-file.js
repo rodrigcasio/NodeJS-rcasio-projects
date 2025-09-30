f4832a1eac36a6acb5ed4304542004916255e42e
rodrigcasio <rod.casio@outlook.com>
2025-09-29 20:10:37 -0600

    Added folder "fs-file-system-practice/" to add a request to the OS to read a file

---
+++ personalNodePractice/fs-file-system-practice/read-file.js
@@ -0,0 +1,25 @@
+// Asynchronous file read (non-blocking I/O)
+// Disk I/O : The request stays on your machine
+// Asking the OS to find the file on the hard drive
+
+const fs = require('fs');
+
+const fileName = 'notes.txt';
+
+console.log(`Program started. Asking the OS to find and read the file`);
+
+fs.readFile(fileName, 'utf-8', (err, data) => {
+                // This callback runs LATER, after the file is read
+    if(err){
+        console.error(`An error occurred:`, err.message);
+        return;
+    }
+    console.log('3. Data recieved (callback executed).');
+    console.log('--- File contents ---');
+    console.log(data);
+    console.log('---------------------');
+
+});
+
+// 2. This line runs IMMEDIATELY, confirming NON-blocking I/O
+console.log('2. Program continues running other code while the file is being read');
