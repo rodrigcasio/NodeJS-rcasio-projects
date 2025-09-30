a3b03d50ba919e331b92d4717eb640decf1d76d5
rodrigcasio <rod.casio@outlook.com>
2025-09-30 16:49:32 -0600

    Added error handlers and passing the HTTP response message into the main app example

--- personalNodePractice/http-verb-practice/resultCallbackFuncEx/index.js
+++ personalNodePractice/http-verb-practice/resultCallbackFuncEx/index.js
@@ -1,7 +1,37 @@
+// Main application 
 
 let weather = require('./get-weather2');
 let location = 'KSFO';
 
-weather.current(location, (temp_f_) => {
+weather.current(location, (err, temp_f) => {
+    
+    // 1. handle err : (if error exists, log failure and stop)
+    if(err){
+        console.error(`ERROR: Could not fetch weather for ${location}.`)
+        console.error(`Details: ${err.message}`);
+        return;
+    }
+
+    // Step 2. handle success --- (if no error, proceed to use the temperature data)
     console.log(`the current temperature at ${location} is ${temp_f}F`);
 });
+
+
+/*  ANOTHER WAY TO HANDLE ERROR,
+    LOGGIN THE ENTIRE OBJECT ERROR 
+    
+    // handle err : (if error exists, log object error)
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    output: 
+    {
+        errno: -3008,
+        code: 'ENOTFOUND',
+        syscall: 'getaddrinfo',
+        hostname: 'w1.weather.gov'
+    }
+
+*/
