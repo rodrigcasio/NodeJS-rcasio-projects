92216d24dcb09ded73c87927b48103f5474fd25e
rodrigcasio <rod.casio@outlook.com>
2025-10-01 16:40:03 -0600

    Added documentation for the example of using the Axios package

--- personalNodePractice/http-verb-practice/promiseExampleAxios/index.js
+++ personalNodePractice/http-verb-practice/promiseExampleAxios/index.js
@@ -1,5 +1,5 @@
 // Example of using Axios
-// Explanation at the bottom of the code 
+// Explanation in file 'READMEExplanation.md' file 
 
 const axios = require('axios');
 
@@ -11,7 +11,7 @@ const connectURL = (url) => {
                                                 console.log(`3. After axious.get for url (sync)`);
     console.log(req);
     req.then((resp) =>{
-        console.log('4. Fulfilled! for : url (async');
+        console.log('4. Fulfilled! for : url (async)');
         console.log(resp.data);
     })
     .catch((err) => {
@@ -20,28 +20,6 @@ const connectURL = (url) => {
 }
 
 
-connectURL('lol');
-connectURL('jsonplaceholder.typicode.com');
+connectURL('https://jsonplaceholder.typicode.com/posts/1');
+console.log('6. end of script (sync) ');
 
-console.log('6. end of script ');
-
-
-// All the (sync) operations (yout numbered logs and Promise creation) run first, in order.
-// The asynchronous operations (inside .then() and .catch() only run after the HTTP request finish,
-//     so they appeaer later in the output)
-
-// Synchronous code runs top-to-bottom, immediately. 
-// Asynchronous code (like network requests) runs later, after the sync code is done
-
-// OUTPUT :
-
-// 1. Start the script (sync)
-// 2. Calling axious.get for url (sync)
-// 3. After axious.get for url (sync)
-// Promise { <pending> }
-// 2. Calling axious.get for url (sync)
-// 3. After axious.get for url (sync)
-// Promise { <pending> }
-// 6. end of script 
-// 5. Rejected for: url (async)
-// 5. Rejected for: url (async)
