5568abdff31344a2e35939e53bc879f56b29dc21
rodrigcasio <rod.casio@outlook.com>
2025-10-01 14:44:54 -0600

    Added content inside the "/promiseEx1/index.js" to practice a user-defined promise

--- personalNodePractice/fs-file-system-practice/promiseEx1/index.js
+++ personalNodePractice/fs-file-system-practice/promiseEx1/index.js
@@ -5,6 +5,20 @@ let fs = require('fs');
 
 const methCall = new Promise((resolve, reject) => {
     setTimeout(() => {
-        let filename = prompt()
-    })
+        let filename = prompt('What is the name of the file?');
+        try{
+            const data = fs.readFileSync(filename, {encoding: 'utf8', flag: 'r'});
+            resolve(data);
+        } catch (err){
+            reject(err)
+        }
+    }, 3000);
 });
+
+
+console.log(methCall);
+
+methCall.then(
+    (data) => console.log(data),
+    (err) => console.log('Error reading file')
+);
---
+++ personalNodePractice/http-verb-practice/promiseExampleAxios/index.js
@@ -0,0 +1,4 @@
+// Example of using Axios
+
+const axios = require('axios');
+
