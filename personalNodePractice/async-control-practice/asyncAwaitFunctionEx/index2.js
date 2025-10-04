6f1bd65c9092965ee2c7d26888dcf5f169082462
rodrigcasio <rod.casio@outlook.com>
2025-10-03 18:53:45 -0600

    Added example simulating an asyncrhonouse operation that returns a Promise

---
+++ personalNodePractice/async-control-practice/asyncAwaitFunctionEx/index2.js
@@ -0,0 +1,32 @@
+// Another example using async/await function
+
+const fetchData = async (delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(typeof delay === 'number' && delay > 0){
+                resolve(`Data fetched after ${delay} miliseconds ✔`);
+            } else{
+                reject(new Error('Operation Failed, missing argument or wrong input user'));
+            }
+        }, delay)
+    });
+}
+
+const processData = async () => {
+    console.log(`1. Starting data processing...`);      //  (sync)
+    try {
+        const result1 = await fetchData(4000);
+        console.log(result1);  
+        
+        const result2 = await fetchData(3000);
+        console.log(result2);
+        console.log(`Data Processed Complete ✅`);
+    } catch (err){
+        console.error(`Data not processed`, err.message);
+    }  
+}
+
+// calling the async function
+processData();
+
+console.log(`2. Please wait...`);       // (sync)
