4d8dea9b7f14eec0370704daa042d1abf7f814f9
rodrigcasio <rod.casio@outlook.com>
2025-10-10 17:21:01 -0600

    Modified order of code placement, placing the app-level middleware `mounting` the routers: `app.use(/shop, shopRouter)`

--- personalNodePractice/express-APIs-practice/appli-middleware-API-3/index.js
+++ personalNodePractice/express-APIs-practice/appli-middleware-API-3/index.js
@@ -1,5 +1,6 @@
-// main app
+// main app (index.js)
 // Example of  implementing using Application-level middleware
+// (global gatekeeper for the API)
 // and adding a router-level in ./shopRoute
 
 const express = require('express');
@@ -8,9 +9,7 @@ const port = 3000;
 
 const shopRouter = require('./shopRoute');
 
-app.use('/shop', shopRouter);
-
-// app-level middleware 
+// app-level middleware     1. mouting the app-level middlerware FIRST
 app.use((req, res, next) => {
     if (req.query.password !== "pwd1234"){
         return res.status(402).send('The user cannot login, please enter password in URL');
@@ -20,6 +19,10 @@ app.use((req, res, next) => {
     next();
 });
 
+// 2. then mount the router, so the pwd check protects it
+app.use('/shop', shopRouter);
+
+//3. also protected !
 app.get('/', (req, res) => {
     res.send('Welcome, you are in the main Home page ☻');
 });
