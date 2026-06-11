a6b9b744b6385bbe2c17f57dc119caccd3423318
rodrigcasio <rod.casio@outlook.com>
2026-06-10 19:02:37 -0600

    `feat:` Include config file for SwaggerEditor website to show interface of the APIs used
    Also completed exercise by iadding within file `HelloInDifferentLanguagesService.js` the correct response for endpoing `/greetings` for current express.js REST API

--- personalNodePractice/swagger-express-APIs-practice/nodejs-server-generated/nodejs-server-server/service/HelloInDifferentLanguagesService.js
+++ personalNodePractice/swagger-express-APIs-practice/nodejs-server-generated/nodejs-server-server/service/HelloInDifferentLanguagesService.js
@@ -9,7 +9,23 @@
  **/
 exports.greetingsGET = function() {
   return new Promise(function(resolve, reject) {
-    resolve();
+
+    let hellos = {
+      "English": "hello",
+      "Hindi": "namastey",
+      "Spanish": "hola",
+      "French": "bonjour",
+      "German": "guten tag",
+      "Italian": "salve",
+      "Chinese": "nǐn hǎo",
+      "Portuguese": "olá",
+      "Arabic": "asalaam alaikum",
+      "Japanese": "konnichiwa",
+      "Korean": "anyoung haseyo",
+      "Russian": "Zdravstvuyte"
+    }
+
+    resolve(hellos);
   });
 }
 
