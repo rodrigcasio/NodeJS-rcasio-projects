cf5ca3cfc328bcd3ba3308904c4b72536271b46a
rodrigcasio <rod.casio@outlook.com>
2026-06-10 18:51:34 -0600

    `feat:` Download server setup and include instructions for completing task2 `creating swagger documentation and generating server code`

---
+++ personalNodePractice/swagger-express-APIs-practice/nodejs-server-generated/nodejs-server-server/service/HelloInDifferentLanguagesService.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+/**
+ * Returns a list of Greetings
+ * Returns greetings in different languages
+ *
+ * no response value expected for this operation
+ **/
+exports.greetingsGET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
